e6209ab9a907c45ee927f7b5734f2a2f9fb3bce3
Aidan Duff <aidanpatrickduff@gmail.com>
2020-07-25 19:56:32 +0100

    Add Decoder class
    In preparation for removing shortURL from the database and generating it
    on the fly
    
    Removed final extra multiplication step from Encoder to simplify.
    Reversed the final string to deal with the decode process

--- url-shortener/src/main/java/com/aidanduff/urlshortener/util/Encoder.java
+++ url-shortener/src/main/java/com/aidanduff/urlshortener/util/Encoder.java
@@ -15,7 +15,24 @@ public class Encoder {
 			sb.append(BASE62[n % 62]);
 			n /= 62;
 		}
-		return sb.toString();
+		return sb.reverse().toString();
+	}
+
+	public int convertBase62ToDecimal(String str) {
+		int id = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') {
+				id = id * 62 + str.charAt(i) - 'a';
+			}
+			if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
+				id = id * 62 + str.charAt(i) - 'A' + 26;
+			}
+			if ('0' <= str.charAt(i) && str.charAt(i) <= '9') {
+				id = id * 62 + str.charAt(i) - '0' + 52;
+			}
+		}
+		return id;
+
 	}
 
 }
