cc57dccd4a6083c482f0a8461264bc5a22ab012d
Aidan Duff <aidanpatrickduff@gmail.com>
2020-08-01 11:16:36 +0100

    Removed code related to the stats API as it's no longer a requirement and it made the code very messy. A completely separate, and more reusable application for this purpose would be much better.

--- url-shortener/src/main/java/com/aidanduff/urlshortener/controller/UrlController.java
+++ url-shortener/src/main/java/com/aidanduff/urlshortener/controller/UrlController.java
@@ -14,39 +14,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aidanduff.urlshortener.model.Stats;
 import com.aidanduff.urlshortener.model.Url;
 import com.aidanduff.urlshortener.model.UrlResponseObject;
 import com.aidanduff.urlshortener.service.UrlService;
 import com.aidanduff.urlshortener.util.Decoder;
 import com.aidanduff.urlshortener.util.Encoder;
-import com.aidanduff.urlshortener.util.StatsHelper;
 
 @RestController
 public class UrlController {
 
 	@Autowired
 	private UrlService urlService;
-	private Stats stats;
-	private int numberOfUrlsShortened;
-	private StatsHelper statsHelper;
 
 	@GetMapping("/")
 	public String welcome() {
-		stats = stats.getInstance();
 		return "Welcome to squeez.it\n\nAdd your long link as plain text in the body and post it to squeez.it\n\n"
-				+ "Paste your short link in the address bar after squeez.it/ to be redirected to your destination\n\n"
-				+ "Statistics are available at /stats";
+				+ "Paste your short link in the address bar after squeez.it/ to be redirected to your destination\n\n";
 	}
 
 	@PostMapping("/squeez.it")
-	public ResponseEntity<UrlResponseObject> addAndEncode(@Validated @RequestBody String originalUrl) {
-		stats = stats.getInstance();
-		statsHelper = new StatsHelper();
-		statsHelper.checkIfLongest(originalUrl);
-		statsHelper.checkIfShortest(originalUrl);
-		stats.setNumberOfUrlsShortened(++numberOfUrlsShortened);
-		
+	public ResponseEntity<UrlResponseObject> addAndEncode(@Validated @RequestBody String originalUrl) {		
 		Url urlToAdd = new Url();
 		
 		if(urlService.getUrl(originalUrl) == null) {
@@ -64,7 +51,6 @@ public class UrlController {
 	@GetMapping("/squeez.it/{shortString}")
 	public void getRedirect(HttpServletResponse httpServletResponse, @PathVariable String shortString)
 			throws IOException {
-		stats = stats.getInstance();
 		httpServletResponse.sendRedirect(urlService.getUrlById(new Decoder().decode(shortString)).getOriginalUrl());
 	}
 }
